bb813b103ae1680f97c7f8a74252d0052b6dabb7
smazik02 <s.mazik2002@gmail.com>
2025-01-03 15:51:13 +0100

    esp full middleware

--- ESP8266/SafeEntry_ESP/src/main.cpp
+++ ESP8266/SafeEntry_ESP/src/main.cpp
@@ -2,54 +2,76 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 
-const char* ssid = "UPC249454130";
-const char* password = "YH97V54Q";
+const char* ssid = "PiwnicaIT";
+const char* password = "bW*5&72xz#kw";
 
-const char* serverName = "http://stachurpi.local:5050/enter?card=BBBB";
-
-// Timer set to 10 minutes (600000)
-// unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
+const char* serverName = "http://stachurpi.local:5000/";
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     WiFi.begin(ssid, password);
-    Serial.println("Connecting");
+    Serial.println("#Connecting");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-    Serial.println("");
-    Serial.print("Connected to WiFi network with IP Address: ");
-    Serial.println(WiFi.localIP());
-
-    Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+    Serial.println("#");
+    Serial.println("#Connected to WiFi");
 }
 
 void loop() {
-    static unsigned long lastTime = 0;
-    if ((millis() - lastTime) > timerDelay) {
-        if (WiFi.status() == WL_CONNECTED) {
-            WiFiClient client;
-            HTTPClient http;
-
-            http.begin(client, serverName);
-
-            // If you need an HTTP request with a content type: text/plain
-            http.addHeader("Content-Type", "text/plain");
-            int httpResponseCode = http.GET();
-
-            Serial.print("HTTP Response code: ");
-            Serial.println(httpResponseCode);
-            Serial.println(http.getString());
-
-            // Free resources
-            http.end();
-        } else {
-            Serial.println("WiFi Disconnected");
-        }
-        lastTime = millis();
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("#WiFi Disconnected");
+        while (true);
     }
+
+    while (Serial.available() <= 0);
+
+    String incoming = Serial.readStringUntil('\n');
+
+    if (incoming[0] == '#') {
+        return;
+    }
+
+    int separator = incoming.indexOf("|");
+    if (separator == -1) {
+        Serial.println("#" + incoming);
+        Serial.println("#No sep found");
+        return;
+    }
+
+    String command = incoming.substring(0, separator);
+    if (!(command == "enter" || command == "exit")) {
+        Serial.println("#Command not supported");
+        return;
+    }
+
+    String cardId = incoming.substring(separator + 1);
+    cardId.trim();
+    if (cardId == "") {
+        Serial.println("#No cardId");
+        return;
+    }
+
+    WiFiClient client;
+    HTTPClient http;
+
+    String connectionUrl = String(serverName) + command + "?card=" + cardId;
+
+    http.begin(client, connectionUrl);
+
+    http.addHeader("Content-Type", "text/plain");
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode != 200) {
+        Serial.println("#Response " + httpResponseCode);
+        return;
+    }
+
+    Serial.print("#HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    Serial.println(http.getString());
+
+    http.end();
 }
