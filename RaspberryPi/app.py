257647fea1b6027e642381d9c55350fb3a013d2a
smazik02 <s.mazik2002@gmail.com>
2025-01-08 21:20:48 +0100

    base client panel

--- RaspberryPi/app.py
+++ RaspberryPi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, make_response
 import sqlite3
 import logging
 
@@ -6,8 +6,22 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=["GET"])
-def hello_world():
-    return '<p>Hello World!</p>'
+def main_page():
+    try:
+        with sqlite3.connect('database.db') as connect:
+            cursor = connect.cursor()
+            cursor.execute('SELECT id, isLocked, inRoom FROM ACCESS_CARDS')
+            access_cards = cursor.fetchall()
+
+            cursor.execute(
+                'SELECT id, accessCard, attemptTime, wasAccepted, reason FROM ACCESS_ATTEMPTS')
+            access_attempts = cursor.fetchall()
+
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        return render_template('error.html', error_msg=e)
+
+    return render_template('index.html', access_cards=access_cards, access_attempts=access_attempts)
 
 
 @app.route('/enter', methods=["GET"])
@@ -72,6 +86,72 @@ def exit():
     except sqlite3.OperationalError as e:
         logging.error(e)
         return '0'
+
+
+@app.route('/api/access_card/<card_id>', methods=["PATCH"])
+def lock_card(card_id):
+    logging.debug(f"locking/unlocking access card: {card_id}")
+
+    try:
+        with sqlite3.connect('database.db') as connect:
+            cursor = connect.cursor()
+            cursor.execute(
+                'SELECT isLocked FROM ACCESS_CARDS WHERE id = ?', (card_id,))
+            is_locked = cursor.fetchone()[0]
+            new_lock = 0 if is_locked == 1 else 1
+            cursor.execute(
+                'UPDATE ACCESS_CARDS SET isLocked = ? WHERE id = ?', (new_lock, card_id))
+
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        return "", 400
+
+    return "", 200
+
+
+@app.route('/api/access_card/<card_id>', methods=["DELETE"])
+def delete_card(card_id):
+    logging.debug(f"removing access card: {card_id}")
+
+    try:
+        with sqlite3.connect('database.db') as connect:
+            cursor = connect.cursor()
+            cursor.execute(
+                'DELETE FROM ACCESS_CARDS WHERE id = ?', (card_id,))
+
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        return "", 400
+
+    response = make_response("", 200)
+    response.headers['HX-Refresh'] = 'true'
+    return response
+
+
+@app.route('/api/access_card/', methods=["POST"])
+def add_card():
+    logging.debug(f"adding access card")
+
+    card_id = request.form.get('ac_code')
+    is_locked = 1 if request.form.get('ac_locked') == 'on' else 0
+
+    try:
+        with sqlite3.connect('database.db') as connect:
+            cursor = connect.cursor()
+            cursor.execute(
+                'SELECT 1 FROM ACCESS_CARDS WHERE id = ?', (card_id,))
+            if len(cursor.fetchall()) > 0:
+                return "", 400
+            cursor.execute(
+                'INSERT INTO ACCESS_CARDS (id, isLocked, inRoom) VALUES (?, ?, 0)', (card_id, is_locked))
+
+    except sqlite3.OperationalError as e:
+        logging.error(e)
+        return "", 400
+
+    response = make_response("", 201)
+    response.headers['HX-Refresh'] = 'true'
+    return response
 
 
 if __name__ == '__main__':
